29192585ecae46026eda73b880042d0e2cdef3fb
isaac <isaac@rubenfamily.com>
2021-02-09 19:14:02 -0800

    reduce loc for piece

--- Piece.java
+++ Piece.java
@@ -1,11 +1,11 @@
 public class Piece {
-    private String color; 
-    private String name = "???"; 
+    private String color;
+    private String name = "???";
     private int x = 0;
     private int y = 0;
-    public String toString(){
-        String nameReturn = name.substring(0,1);
-        return nameReturn.toUpperCase() ; 
+
+    public String toString() {
+        return name.substring(0, 1).toUpperCase();
     }
 
 }
