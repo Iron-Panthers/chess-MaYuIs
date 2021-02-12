8daa496cd8faa42abb3e9117937124f485773750
isaac <isaac@rubenfamily.com>
2021-02-11 18:21:48 -0800

    cord object, more piece methods

--- Piece.java
+++ Piece.java
@@ -4,6 +4,15 @@ public class Piece {
     private int x = 0;
     private int y = 0;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
     public String toString() {
         return name.substring(0, 1).toUpperCase();
     }
