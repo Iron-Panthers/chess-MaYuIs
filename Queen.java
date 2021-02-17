e5c74d1dd7a89254a6656c111eb004d202d38d06
Yusef <24mohammedtahery14@stu.smuhsd.org>
2021-02-16 18:30:00 -0800

    Creating Queen Cords

--- Queen.java
+++ Queen.java
@@ -1,4 +1,15 @@
 public class Queen extends Piece {
     private String name = "queen";
-    
+    private Cord[] moves = {
+        new Cord(0,-1),
+        new Cord(1,-1),
+        new Cord(1,0),
+        new Cord(1,1),
+        //negative is right, positive is left
+        //finish the cords
+    };
+    private Cord[] takes = moves;
+    private boolean ray = true;
+
 }
+
