a3372d972b207c5e9f45161008a2dcbdba42ec9e
maddie <maddie.behr2007@gmail.com>
2021-02-18 17:45:23 -0800

    fix big whoops
    knight class fixed my bad

--- Knight.java
+++ Knight.java
@@ -5,10 +5,10 @@ public class Knight extends Piece{// + left + - 3ight
         new Cord(-3,-1),// up 1 right 3
         new Cord(3,1),// down 1 left 3
         new Cord(-3,1),// down 1 right 3
-        new Cord(3,1),// down 3 left 1
-        new Cord(-3,-1),// up 3  left 1
-        new Cord(3,1),// down 3 right 1
-        new Cord(-3,-1)// up 3  right 1
+        new Cord(1,3),// down 3 left 1
+        new Cord(-1,-3),// up 3  left 1
+        new Cord(-1,3),// down 3 right 1
+        new Cord(1,-3)// up 3  right 1
         // all of the moves 
     };// all moves within 
     private boolean ray = false;
