c8c6b70f7296e78038d1c1b6e45c9e04f6cb8f9c
isaac <isaac@rubenfamily.com>
2021-02-18 18:49:46 -0800

    print a larger board for readability

--- Board.java
+++ Board.java
@@ -22,22 +22,39 @@ public class Board {
     public String toString() {
         StringBuilder ret = new StringBuilder();/* String builder is just adds to the end of string */
         for (int y = 0, cord = HEIGHT; y < HEIGHT; y++, cord--) {
-            ret.append(cord + " ");// appened = add on to the end
+            ret.append("  ");
             for (int x = 0; x < WIDTH; x++) {
                 ret.append((x + y) % 2 == 0 ? CC.BLACK_BACKGROUND : CC.WHITE_BACKGROUND);
+                ret.append("      ");
+                ret.append(CC.RESET);
+            }
+            ret.append("\n");
+            ret.append(cord);// appened = add on to the end
+            ret.append(" ");
+            for (int x = 0; x < WIDTH; x++) {
+                ret.append((x + y) % 2 == 0 ? CC.BLACK_BACKGROUND : CC.WHITE_BACKGROUND);
+                ret.append("  ");
                 ret.append(CC.RED_BOLD_BRIGHT);
                 ret.append(this.get(x, y) != null ? this.get(x, y).toString() : " ");
-                ret.append(" ");
+                ret.append("   ");
                 ret.append(CC.RESET);
                 /*
                  * null = nothing there ? if statment in one line
                  */
             }
-            ret.append("\n");// making new line = \n
+            ret.append("\n  ");// making new line = \n
+            for (int x = 0; x < WIDTH; x++) {
+                ret.append((x + y) % 2 == 0 ? CC.BLACK_BACKGROUND : CC.WHITE_BACKGROUND);
+                ret.append("      ");
+                ret.append(CC.RESET);
+            }
+            ret.append("\n");
         }
         ret.append("  ");// nothing to draw between 1 and a
         for (int ch = 97; ch < 97 + WIDTH; ch++) { // 97 = beginning of the lower case alphabet
-            ret.append((char) ch + " ");
+            ret.append("  ");
+            ret.append((char) ch);
+            ret.append("   ");
         }
         ret.append("  \n");
         return ret.toString();
