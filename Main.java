488dd68e59d09380ff26dcdd4119597d53f2f785
isaac <isaac@rubenfamily.com>
2021-02-23 18:58:07 -0800

    reduce line count, increase clarity, add missing pieces

--- Main.java
+++ Main.java
@@ -1,21 +1,24 @@
 class Main {
     public static void main(String[] args) {
         Board board = new Board();
-        for (int i = 0; i < 8; i++) {
-            board.set(i, 1, new Pawn(Color.WHITE));
-        }
-        for (int i = 0; i < 8; i++) {
-            board.set(i, 6, new Pawn(Color.BLACK));
-        }
-        board.set(3, 0, new Queen(Color.WHITE));
-        board.set(3, 7, new Queen(Color.BLACK));
-        board.set(7, 0, new Rook(Color.WHITE));
-        board.set(0, 0, new Rook(Color.WHITE));
-        board.set(7, 7, new Rook(Color.BLACK));
-        board.set(0, 7, new Rook(Color.BLACK));
-        board.set(6, 7, new Knight(Color.BLACK));
-        board.set(1, 7, new Knight(Color.BLACK));
+        for (int y = 0; y < 8; y += 7) {
+            Color color = y == 0 ? Color.BLACK : Color.WHITE;
+            for (int x = 0; x < 8; x++) {
+                board.set(x, y == 0 ? 1 : 6, new Pawn(color));
+            }
+            board.set(3, y, new Queen(color));
+            board.set(4, y, new King(color));
+            
+            board.set(7, y, new Rook(color));
+            board.set(0, y, new Rook(color));
+            
+            board.set(6, y, new Knight(color));
+            board.set(1, y, new Knight(color));
 
+            board.set(5, y, new Bishop(color));
+            board.set(2, y, new Bishop(color));
+        }
+        
         System.out.print(board);
     }
 }
